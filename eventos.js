f7451ec622df9b352d36e8771188a4ed95a19adf
maral@Alejandra <mar.alejandra.silva@gmail.com>
2024-08-25 22:28:31 -0300

    addEventListener

--- eventos.js
+++ eventos.js
@@ -1,2 +1,7 @@
- 
- 
+document.addEventListener("DOMContentLoaded", function() {
+    let styledDiv = document.querySelector('.styled-div');
+
+    styledDiv.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
